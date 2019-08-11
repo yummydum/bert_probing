1281c3ba212e916d3d460481f6e72d2bb6cf0c5e
yummydum <atsushisumita0421@gmail.com>
2019-08-11 19:25:05 +0900

    Complete preprocess/train script for BERT probing

--- ST_probe.py
+++ ST_probe.py
@@ -5,181 +5,193 @@ For Hydrogen;
 
 Probe BERT hidden representations for POS tagging and dependency parsing (Linguistic correlation analysis)
 """
+
 import argparse
 import logging
-import csv
+import json
+from pathlib import Path
 import numpy as np
 from joblib import dump, load
-from sklearn.linear_model import SGDClassifier
-from pathlib import Path
 import torch
 from pytorch_transformers import BertConfig,BertModel, BertTokenizer
+from sklearn.linear_model import SGDClassifier
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(process)d-%(asctime)s-%(levelname)s-%(message)s',
-                    datefmt='%d-%b-%y %H:%M:%S')
+# Load model
+model_type = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(model_type)
+config = BertConfig.from_pretrained(model_type)
+config.output_hidden_states = True
+config.output_attentions = True
+model = BertModel(config)
+model.eval()
 
-def clean_sentence(sentence):
-    # TODO Delete this part
-    sentence = sentence.replace("NUM","0")
-    sentence = sentence.replace('*root* ','')
-    return sentence.replace("<quote>",'"')
+def train(args):
+    # Read the datas
+    train_path = Path(f"probing_data/ST/ST-train.json")
+    with train_path.open(mode="r") as f:
+        train_data = json.load(f)
 
-def train_probing_classifier(args):
+    # Get class num
+    if args.target == "position" or args.target == "head":
+        class_num = range(train_data["max_sentence_len"])
+    elif args.target == "pos":
+        class_num = range(len(train_data["all_pos"]))
 
-    with torch.no_grad():
-        # Load model
-        model_type = 'bert-base-uncased'
-        tokenizer = BertTokenizer.from_pretrained(model_type)
-        config = BertConfig.from_pretrained(model_type)
-        config.output_hidden_states = True
-        config.output_attentions = True
-        model = BertModel(config)
-        model.eval()
-
-        # Convert the sentence in ST to BERT hidden rep
-        st_path = Path("probing_data/ST/train.tsv")
-        probing_models = dict()
-        for i in range(13):  # Num layer == 13
-            probing_models[i] = SGDClassifier(penalty="elasticnet")
-
-        # with st_path.open(mode="r") as f:
-        #     next(f)
-        #     row = next(f).split("\t")
-
-        with st_path.open(mode="r") as f:
-            next(f)
-            for row_num,row in enumerate(csv.reader(f,delimiter="\t")):  # sentence = "[CLS] I love you [SEP]"
-                # Reformat the data
-                cleaned_sentence = clean_sentence(row[0])
-                sentence = f"[CLS] {cleaned_sentence} [SEP]"
-                original_token_list = cleaned_sentence.split(" ")
-
-                # Forward
-                tokenized = tokenizer.tokenize(sentence)
-                encoded = tokenizer.encode(sentence)
-                tensor = torch.LongTensor(encoded).reshape(1,len(encoded))
-                last_hid,pooler,all_hid,all_attention = model(tensor)
-
-                # Make feature for each layer each token
-                all_hid_list = [all_hid[i][0].numpy() for i in range(len(all_hid))]
-                for i in range(len(all_hid_list)): # i = 1
-                    skip_step = 0
-                    cum_skip_step = 0
-                    X,y = [],[]
-                    for j in range(len(tokenized)):  # j = 1
-                        # If there was a concatenation in the prev loop, skip
-                        if skip_step > 0:
-                            skip_step -= 1
-                            cum_skip_step += 1
-                            continue
-                        # Special token
-                        if tokenized[j] in {"[CLS]","[SEP]"}:
-                            continue
-                        else:
-                            logging.debug(f"BPE tokenized:{tokenized[j]}")
-                            logging.debug(f"Original token:{original_token_list[j-cum_skip_step-1]}")
-
-                            # Determine the target variable
-                            if args.target == "position":
-                                target = j
-                            elif args.target == "pos":
-                                target = row[1].split(" ")[j-cum_skip_step]
-                            elif args.target == "head":
-                                target == row[2].split(" ")[j-cum_skip_step]
-                            y.append(pos_tag.index(target))
-
-                            # If the BPE and original token is consistent, write
-                            if tokenized[j] == original_token_list[j-cum_skip_step-1]:
-                                X.append(all_hid_list[i][j])
-                            # Else restore the original token
-                            else:
-                                logging.debug("Inconsistency found")
-                                temp_token_list = [tokenized[j]]
-                                temp_hid_rep_list = all_hid_list[i][j]
-                                for k in range(j+1,len(tokenized)):
-                                    temp_token_list.append(tokenized[k])
-                                    temp_hid_rep_list += all_hid_list[i][k]
-                                    skip_step += 1
-                                    # If the concatenated BPE matches the original token, write the result
-                                    temp = "".join(temp_token_list).replace("##","")
-                                    logging.debug("concatenated:",temp)
-                                    if original_token_list[j-cum_skip_step-1] == temp:
-                                        logging.debug(f"Match! {temp}")
-                                        X.append(temp_hid_rep_list / len(temp_token_list))
-                                        break
-
-                    # Update param for this sentence
-                    if i == 0:
-                        logging.info(f"Now at {row_num}th sentence")
-                    X = np.array(X)  # X.shape
-                    y = np.array(y)  # y.shape
-                    probing_models[i].partial_fit(X,y,classes=range(len(pos_tag)))
-                    if args.test_mode and row_num > 10:
-                        for i in range(13):  # Num layer == 13
-                            dump(probing_models[i],f"probing_data/ST/{i}.joblib")
-                        return
-
-    # Finally save the results
+    # Init probing model for the ith layer
+    probing_models = dict()
     for i in range(13):  # Num layer == 13
-        dump(probing_models[i],f"probing_data/ST/{i}.joblib")
+        probing_models[i] = SGDClassifier(penalty="elasticnet")
 
+    # Train loop
+    for epoch in range(args.epoch_num):
+        for example_num,example in train_data["data"].items(): # example = train_data["data"]['1']
+            example_num = int(example_num)
+            # Report progress
+            if example_num % 1000 == 0:
+                logging.info(f"Now at {example_num}th example")
+            # Forward
+            sentence = "[CLS] " + " ".join(example["word"]) + " [SEP]"
+            tokenized = tokenizer.tokenize(sentence)
+            encoded = tokenizer.encode(sentence)
+            tensor = torch.LongTensor(encoded).reshape(1,len(encoded))
+            last_hid,pooler,all_hid,all_attention = model(tensor)
+            all_hid_list = [all_hid[i][0].detach().numpy() for i in range(len(all_hid))]
+
+            # Fit model for ith model
+            for i in range(len(all_hid_list)): # i = 1
+                X,y = extract_X_y(args,
+                                  tokenized,
+                                  sentence.split(" "),
+                                  example,
+                                  all_hid_list[i])
+                # preprocess y
+                if args.target == "pos":
+                    y = np.array([train_data["all_pos"].index(l[0]) for l in y])
+                probing_models[i].partial_fit(X,y,classes=class_num)
+            # Stop here if debug mode
+            if args.debug and example_num > 3:
+                break
+        # Evaluate accuracy on dev data
+        acc = evaluate(args,probing_models,train_data["all_pos"],is_dev=True)
+        logging.info(f"Current accuracy is {acc}")
+        # Save the model for this epoch
+        for i in range(13):  # Num layer == 13
+            dump(probing_models[i],f"probing_data/ST/{i}_{epoch}.joblib")
+
+# all_pos = train_data["all_pos"]
+def evaluate(args,probing_models,all_pos,is_dev):
+    correct = 0
+    total = 0
+    # Read the datas
+    if is_dev:
+        data_path = Path(f"probing_data/ST/ST-dev.json")
+    else:
+        data_path = Path(f"probing_data/ST/ST-test.json")
+    with data_path.open(mode="r") as f:
+        data = json.load(f)
+
+    for example_num,example in data["data"].items(): # example = train_data["data"]['1']
+        example_num = int(example_num)
+        # Report progress
+        if example_num % 1000 == 0:
+            logging.info(f"Now at {example_num}th example")
+        # Forward
+        sentence = "[CLS] " + " ".join(example["word"]) + " [SEP]"
+        tokenized = tokenizer.tokenize(sentence)
+        encoded = tokenizer.encode(sentence)
+        tensor = torch.LongTensor(encoded).reshape(1,len(encoded))
+        last_hid,pooler,all_hid,all_attention = model(tensor)
+        all_hid_list = [all_hid[i][0].detach().numpy() for i in range(len(all_hid))]
+
+        # Fit model for ith model
+        for i in range(len(all_hid_list)): # i = 1
+            X,y = extract_X_y(args,
+                              tokenized,
+                              sentence.split(" "),
+                              example,
+                              all_hid_list[i])
+            y_hat = probing_models[i].predict(X)
+            # postprocess y
+            if args.target == "pos":
+                y_hat =[all_pos[l] for l in y_hat]
+            correct += np.sum(y_hat == y.T)
+            total += len(y_hat)
+        # Stop here if debug mode
+        if args.debug and example_num > 3:
+            break
+
+    return correct / total
+
+# original_tokenized = sentence.split(" "); hidden_i = all_hid_list[i]
+def extract_X_y(args,tokenized,original_tokenized,example,hidden_i):
+    """
+    Resolve the inconsistency between simple tokenization and BPE
+    """
+    # Start loop
+    target = example[args.target]
+    skip_step = 0
+    cum_skip_step = 0
+    X,y = [],[]
+    for j in range(len(tokenized)):  # j = 1
+        # If there was a concatenation in the prev loop, skip
+        if skip_step > 0:
+            skip_step -= 1
+            cum_skip_step += 1
+            continue
+        # Special token
+        if tokenized[j] in {"[CLS]","[SEP]"}:
+            continue
+        else:
+            # -cum_skip_step for number o0f skips
+            logging.debug(f"BPE tokenized:{tokenized[j]}")
+            logging.debug(f"Original token:{original_tokenized[j-cum_skip_step]}")
+
+            # y
+            t = target[j-cum_skip_step-1]
+            y.append([t])
+
+            # If the BPE and original token is consistent, write
+            if tokenized[j] == original_tokenized[j-cum_skip_step]:
+                X.append(hidden_i[j])
+            # Else restore the original token
+            else:
+                logging.debug("Inconsistency found")
+                temp_token_list = [tokenized[j]]
+                temp_hid_rep_list = hidden_i[j]
+                for k in range(j+1,len(tokenized)):  # k = 0
+                    temp_token_list.append(tokenized[k])
+                    temp_hid_rep_list += hidden_i[k]
+                    skip_step += 1
+                    # If the concatenated BPE matches the original token, write the result
+                    temp = "".join(temp_token_list).replace("##","")
+                    logging.debug(f"concatenated:{temp}")
+                    if original_tokenized[j-cum_skip_step] == temp:
+                        logging.debug(f"Match! {temp}")
+                        X.append(temp_hid_rep_list / len(temp_token_list))
+                        break
+
+    return np.array(X),np.array(y)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--test_mode",action='store_true',
-                        help="Test mode if flagged")  # pos class num = 46
-    parser.add_argument("--target", default="pos", type=str,
-                        help="Word position,pos,head")  # pos class num = 46
-    args = parser.parse_args()  # %tb
-    # import sys
-    # sys.argv[0] = ""
-    # del sys
+    parser.add_argument("--debug",action='store_true',
+                        help="Debug mode if flagged")  # pos class num = 46
+    parser.add_argument("--target", default="pos",
+                        type=str,
+                        choices = ["position","pos","head"],
+                        help="The target variable to be predicted")  # pos class num = 46
+    parser.add_argument("--epoch_num",type=int,default=10,
+                        help="Number of epoch")  # pos class num = 46
+    args = parser.parse_args()
 
+    # Set logger
+    if args.debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(level=level,
+                        format='%(process)d-%(asctime)s-%(levelname)s-%(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S')
+    train(args)
 
-    pos_tag = [ 'NN',
-                 'IN',
-                 'NNP',
-                 'DT',
-                 'JJ',
-                 'NNS',
-                 ',',
-                 '.',
-                 'CD',
-                 'RB',
-                 'VBD',
-                 'VB',
-                 'CC',
-                 'VBZ',
-                 'VBN',
-                 'PRP',
-                 'VBG',
-                 'TO',
-                 'VBP',
-                 'MD',
-                 'POS',
-                 'PRP$',
-                 '$',
-                 '``',
-                 "''",
-                 ':',
-                 'WDT',
-                 'JJR',
-                 'NNPS',
-                 'RP',
-                 'WP',
-                 'WRB',
-                 'JJS',
-                 'RBR',
-                 ')',
-                 '(',
-                 'EX',
-                 'RBS',
-                 'PDT',
-                 'FW',
-                 'WP$',
-                 '#',
-                 'UH',
-                 'SYM',
-                 'LS']
-    train_probing_classifier(args)
+    # args = parser.parse_args(["--debug"])
---
+++ process_ST.py
@@ -0,0 +1,84 @@
+"""
+For Hydrogen;
+%load_ext autoreload
+%autoreload 2
+"""
+
+import argparse
+from pathlib import Path
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(process)d-%(asctime)s-%(levelname)s-%(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
+def main(args):
+
+    # Init the dicts
+    result_dict = dict()
+    result_dict["max_sentence_len"] = 0
+    result_dict["data"] = dict()
+    for i in ["all_pos","all_cpos","all_relation"]:
+        result_dict[i] = set()
+    entry_dict = dict()
+    for key in ["position","word","cpos","pos","head","relation"]:
+        entry_dict[key] = []
+
+    count = 0
+    conll_path = Path(f"probing_data/ST/en-universal-{args.which}.conll")
+    with conll_path.open(mode="r",encoding="utf-8") as f: # f = conll_path.open(), f.close()
+        for line in f:   # line = next(f)
+            tok = line.strip().split('\t')
+            logging.debug(tok)
+            # If empty line, write the result
+            if not tok or line.strip() == '':
+                logging.debug(tok)
+                logging.debug(entry_dict)
+                if len(entry_dict["word"])>1:
+                    result_dict["data"][count] = entry_dict.copy()
+                    count += 1
+                    if len(entry_dict["word"]) > result_dict["max_sentence_len"]:
+                        result_dict["max_sentence_len"] = len(entry_dict["word"])
+                # Init entry dict
+                for key in entry_dict.keys():
+                    entry_dict[key] = []
+                # Report progress
+                if count >= 5000 and count % 5000 == 0:
+                    logging.info(f"Now at {count}th example")
+                if args.debug and count > 5:
+                    break
+            # else continue constructing the entry list
+            else:
+                entry_dict["position"].append(int(tok[0]))
+                entry_dict["word"].append(tok[1].lower())
+                entry_dict["cpos"].append(tok[3].upper())
+                entry_dict["pos"].append(tok[4].upper())
+                entry_dict["cpos"].append(tok[5])
+                entry_dict["head"].append(int(tok[6]))
+                entry_dict["relation"].append(tok[7])
+                # Track all pos/relation and max sentence len
+                result_dict["all_cpos"].add(tok[3].upper())
+                result_dict["all_pos"].add(tok[4].upper())
+                result_dict["all_relation"].add(tok[7])
+
+        # End of loop, yield the last sentence
+        if len(entry_dict) > 1:
+            result_dict["data"][count] = entry_dict
+
+    # Dump the result into json
+    for i in ["all_pos","all_cpos","all_relation"]:
+        result_dict[i] = list(result_dict[i])
+    result_path = Path(f"probing_data/ST/ST-{args.which}.json")
+    logging.info(f"Now writing the result to {result_path}")
+    with result_path.open(mode="w") as f:
+        json.dump(result_dict,f)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("which",type=str,
+                        help="Train or dev or test")
+    parser.add_argument("--debug",action='store_true',
+                        help="Debug mode if flagged")
+    args = parser.parse_args()
+    main(args)
